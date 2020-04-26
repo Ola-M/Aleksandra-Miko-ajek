693f88e5c4ba991bd86d56aa0d2167bf60c9f86a
Ola-M <aleksandra.mikol99@gmail.com>
2020-04-27 01:25:01 +0200

    27.04.2020

---
+++ pw/Wizytowka/Assets/wyswiietl.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using System;
+
+public class wyswiietl : MonoBehaviour
+{
+    public int wyswietl = 10;
+    public void Start()
+    {
+        for(int i = 0; i<wyswietl; i++)
+        {
+            Debug.Log("Aleksandra Mikołajek");
+        }
+    }
+ }
