d7767c5d0da15859ed7180fedb50c665b1df37fe
Ola-M <aleksandra.mikol99@gmail.com>
2020-05-12 19:21:35 +0200

    12,05,2020

---
+++ pipo2/lab6_1/lab6_1/Silnia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1
+{
+   public class Silnia
+    {
+        private int liczba;
+        public int Result()
+        {
+            int y=1;
+            for(int i = liczba; i>0 ; i--)
+            {
+               y*= i;
+            }
+            return y;
+        }
+        
+        public int  GetLiczba()
+        {
+            return liczba;
+        }
+        public void SetLiczba(int n)
+        {
+            liczba = n;
+        }
+
+
+    }
+}
---
+++ pipo2/lab6_1/test1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab6_1;
+
+
+namespace test1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Silnia_0()
+        {
+            Silnia silnia = new Silnia();
+            silnia.SetLiczba(0);
+            Assert.AreEqual(silnia.Result(), 1);
+        }
+
+        [TestMethod]
+        public void Silnia_1()
+        {
+            Silnia silnia = new Silnia();
+            silnia.SetLiczba(1);
+            Assert.AreEqual(silnia.Result(), 1);
+        }
+        [TestMethod]
+        public void Silnia_5()
+        {
+            Silnia silnia = new Silnia();
+            silnia.SetLiczba(5);
+            Assert.AreEqual(silnia.Result(), 120);
+        }
+    }
+}
