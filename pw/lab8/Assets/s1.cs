fd47ec1884c6a5c2f6f9a82ec63bd1f98ee81bd8
Ola-M <aleksandra.mikol99@gmail.com>
2020-05-23 21:18:50 +0200

    lab8

---
+++ pw/lab8/Assets/s1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour   
+{
+    GameObject[] game;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        game = GameObject.FindGameObjectsWithTag("kula");
+        foreach(GameObject s in game)
+        {
+            Destroy(s.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ pw/lab8/Assets/skrypt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skrypt : MonoBehaviour
+{
+    public float cube;
+    public float cube1;
+    public float cube2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(cube, cube1, cube2);
+        
+    }
+}
