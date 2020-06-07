6d1e544c05611b9ba4bf3089532d3218b7393e8a
Ola-M <aleksandra.mikol99@gmail.com>
2020-06-07 15:58:33 +0200

    07,06,2020

---
+++ pw/lab9/Assets/CreateObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+  
+
+
+
+    public GameObject[] kulka;
+
+    
+
+    void Start()
+    {
+        InvokeRepeating("GenerateObject", 4f, 3f);
+    }
+
+    
+    void Update()
+    {
+        
+        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            CancelInvoke("GenerateObject");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+             Instantiate(kulka[0].gameObject, transform.position, Quaternion.identity);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Instantiate(kulka[1].gameObject, transform.position, Quaternion.identity);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+             Instantiate(kulka[2].gameObject, transform.position, Quaternion.identity);
+        }
+         if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Instantiate(kulka[3].gameObject, transform.position, Quaternion.identity); 
+        }
+         if(Input.GetKeyDown(KeyCode.Alpha5))
+        {
+             Instantiate(kulka[4].gameObject, transform.position, Quaternion.identity);
+        }
+         if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateObject();
+        }
+       
+    }
+
+    void GenerateObject()
+    {
+        int los = Random.Range(0, kulka.Length);
+        Instantiate(kulka[los].gameObject, transform.position, Quaternion.identity);
+    }
+}
+ 
+   
+
