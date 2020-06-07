642b097d28d5b8cc3dafbcc8909daac132aaa935
Ola-M <aleksandra.mikol99@gmail.com>
2020-06-07 21:48:42 +0200

    07,06,2020
    uuhybtgvf

---
+++ pw/WizualneProjekt/Assets/lava.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lava : MonoBehaviour
+{
+  public GameObject kulka;
+
+
+    
+
+    void Start()
+    {
+    
+        InvokeRepeating("GenerateObject", 4f, 1f);
+    }
+
+    
+    void Update()
+    {
+       
+       
+    }
+
+    void GenerateObject()
+    {
+        float Valuex = Random.Range(-900, 900);
+        float Valuey = Random.Range(-900, 900);
+        GameObject bum = Instantiate(kulka, transform.position, Quaternion.identity);
+        bum.GetComponent<Rigidbody>().AddForce(new Vector3(Valuex,900f,Valuey));
+    }
+}
